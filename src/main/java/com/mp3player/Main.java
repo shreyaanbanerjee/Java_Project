95b4740bd13e97d0d8309116a8cc95dc803d7662
Shlok <shloksachdev25@gmail.com>
2025-04-02 16:06:06 +0530

    removed playlist directory

--- src/main/java/com/mp3player/Main.java
+++ src/main/java/com/mp3player/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         String mp3Directory = "./songs";
-        String playlistsDirectory = "./playlists";
+        
 
         TerminalUI ui = new TerminalUI(mp3Directory, playlistsDirectory);
         ui.start();
